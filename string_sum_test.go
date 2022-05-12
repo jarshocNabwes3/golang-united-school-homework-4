bf74884366a8e2400f68f38a874382c4fefca03b
Peter Vereshagin <peter@vereshagin.org>
2022-05-12 17:36:01 +0000

    feat: :rocket: init

--- string_sum_test.go
+++ string_sum_test.go
@@ -2,8 +2,12 @@ package string_sum
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStringSum(t *testing.T) {
-
+	result, err := StringSum(``)
+	assert.EqualErrorf(t, err, errorEmptyInput.Error(), "Error should be: %v, got: %v", errorEmptyInput.Error(), err)
+	assert.Equal(t, ``, result, `Result string differs from expected: Input: '%v'; Expected: '%v'; Result: '%v'`, ``, ``, result)
 }
