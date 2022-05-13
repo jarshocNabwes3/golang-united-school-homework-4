e887d591632fcac31aab05daf540d438ea58e0bf
Peter Vereshagin <peter@vereshagin.org>
2022-05-13 06:38:10 +0000

    refactor: :art: name

--- string_sum_test.go
+++ string_sum_test.go
@@ -6,15 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testEmptyResultAndError(t *testing.T, input, resultExpected string, errExpected error) {
+func testInOutErr(t *testing.T, input, resultExpected string, errExpected error) {
 	result, err := StringSum(``)
 	assert.EqualErrorf(t, err, errorEmptyInput.Error(), "Input: '%v' Error should be: %v, got: %v", input, errExpected.Error(), err)
 	assert.Equal(t, ``, result, `Result string differs from expected: Input: '%v'; Expected: '%v'; Result: '%v'`, ``, resultExpected, result)
 }
 
 func TestStringSum(t *testing.T) {
-	testEmptyResultAndError(t, ``, ``, errorEmptyInput)
-	testEmptyResultAndError(t, ` `, ``, errorEmptyInput)
-	testEmptyResultAndError(t, ` 
-		``, ``, errorEmptyInput)
+	testInOutErr(t, ``, ``, errorEmptyInput)
+	testInOutErr(t, ` `, ``, errorEmptyInput)
+	testInOutErr(t, ` 
+		`, ``, errorEmptyInput)
 }
