46d6d651e7a387d07ee1d66303608fafc8d78bb4
Peter Vereshagin <peter@vereshagin.org>
2022-05-14 12:28:24 +0000

    feat: :tada: implemented

--- string_sum.go
+++ string_sum.go
@@ -3,7 +3,8 @@ package string_sum
 import (
 	"errors"
 	"fmt"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -11,7 +12,8 @@ var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
-	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorNotTwoOperands     = errors.New("expecting two operands, but received more or less")
+	errorNotSingleOperation = errors.New("expecting one operation, but received")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -24,10 +26,65 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
-func StringSum(input string) (output string, err error) {
-	input = strings.TrimSpace(input)
-	if input == `` {
-		return ``, fmt.Errorf(`sum: %w`, errorEmptyInput)
+func findOperation(input string) (operation []string, err error) {
+
+	input = regexp.MustCompile(`(?ms)[ \t\r]+`).ReplaceAllString(input, ``)
+	inputSingleLine := regexp.MustCompile(`\n`).ReplaceAllString(input, ``)
+	if inputSingleLine == `` {
+		return make([]string, 0), fmt.Errorf(`sum: %w`, errorEmptyInput)
 	}
-	return "", nil
+
+	input = regexp.MustCompile(`(?s)([^\n])$`).ReplaceAllString(input,
+		"$1\n")
+
+	operations := regexp.MustCompile(`([-+]?[^-+\s]+)([-+][^-+\s]+)([-+][^-+\s]+)?[$\n]`).FindAllStringSubmatch(input, 2)
+	if operations == nil {
+		return make([]string, 0), fmt.Errorf(`sum: %w: '%v'`, errorNotSingleOperation, 0)
+	}
+	operationsCount := len(operations)
+	if operationsCount != 1 {
+		return make([]string, 0), fmt.Errorf(`sum: %w: '%v'`, errorNotSingleOperation, operationsCount)
+	}
+	operation = operations[0]
+
+	return
+}
+
+func StringSum(input string) (output string, err error) {
+	operation, err := findOperation(input)
+	if err != nil {
+		return ``, err
+	}
+
+	operationCount := len(operation)
+	if operationCount != 4 {
+		return ``, fmt.Errorf(`sum: %w`, errorNotTwoOperands)
+	}
+	lastOperand := operation[3:4][0]
+	if lastOperand != `` {
+		return ``, fmt.Errorf(`sum: %w`, errorNotTwoOperands)
+	}
+	operands := operation[1:3]
+
+	sum := 0
+	for i := range operands {
+		operand := 0
+
+		operandString := operands[i]
+		operand, err = strconv.Atoi(operandString)
+		if err != nil {
+			err = fmt.Errorf(`sum: strconv Atoi error: '%w', operand: '%v'`, err, operandString)
+			break
+		}
+
+		sum += operand
+	}
+
+	if err != nil {
+		return ``, err
+	}
+
+	output = strconv.Itoa(sum)
+
+	return
 }
